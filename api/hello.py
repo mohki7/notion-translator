d5e06349ac9b1f789d62a9d954f2f1f1ff27c02d
mohki7 <m.ohki7forwork@gmail.com>
2024-10-13 12:42:05 +0900

    Add hello world function

--- api/hello.py
+++ api/hello.py
@@ -1,5 +1,7 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Hello, World!"
-    }
+from http.server import BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
